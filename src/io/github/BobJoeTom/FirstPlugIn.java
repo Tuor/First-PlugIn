a3505004ee25991e68c1929f974b57f0e5894309
BobJoeTom <unconfigured@null.spigotmc.org>
2015-12-19 14:32:58 -0500

    Update

--- src/io/github/BobJoeTom/FirstPlugIn.java
+++ src/io/github/BobJoeTom/FirstPlugIn.java
@@ -1,5 +1,16 @@
 package io.github.BobJoeTom;
+import org.bukkit.plugin.java.*;
 
-public class FirstPlugIn {
+public class FirstPlugIn extends JavaPlugin{
+	
+	
+    @Override
+    public void onEnable() {
+    	getLogger().info("AOSFINOAISNFOASNFONFAS HEEEEEEEEEEEEELLLLLLLLLLLLLOOOOOOOOOOOOOOOOO WORLD");
+    }
+   
+    @Override
+    public void onDisable() {
 
+    }
 }
